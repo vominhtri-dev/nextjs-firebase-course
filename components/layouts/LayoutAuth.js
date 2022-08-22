7c21e7618135063d9418179caf4c8b04b80acefb
Vo Minh Tri <minhtrithcb@gmail.com>
2022-08-22 18:02:04 +0700

    Fix banner text on auth layout

--- components/layouts/LayoutAuth.js
+++ components/layouts/LayoutAuth.js
@@ -1,19 +1,19 @@
-import { Box, Grid, GridItem, Image } from "@chakra-ui/react"
-import Link from "next/link"
-import React from "react"
-import BannerHeading from "../pages/home/BannerHeading"
+import { Box, Grid, GridItem, Image, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+import React from 'react'
+import styles from '../../styles/banner.module.css'
 
 const LayoutAuth = ({ children }) => {
     return (
         <Box>
-            <Grid gridTemplateColumns={"repeat(12,1fr)"}>
+            <Grid gridTemplateColumns={'repeat(12,1fr)'}>
                 <GridItem
-                    colSpan={["0", "0", "4", "6"]}
+                    colSpan={['0', '0', '4', '6']}
                     alignItems='center'
                     minH='100vh'
                     pos='relative'
                     bgColor='#4549e0'
-                    display={["none", "none", "flex"]}
+                    display={['none', 'none', 'flex']}
                     color='white'
                 >
                     <Box
@@ -24,7 +24,7 @@ const LayoutAuth = ({ children }) => {
                         alignItems='center'
                     >
                         <Image
-                            src={"./favicon.png"}
+                            src={'./favicon.png'}
                             width='40px'
                             height='40px'
                             property='fill'
@@ -33,20 +33,45 @@ const LayoutAuth = ({ children }) => {
                         />
                         <Link href='/'>TRIDEV.IO</Link>
                     </Box>
-                    <Box w='80%' mt='0' ml='10%' p='8'>
-                        <BannerHeading
-                            text='Tridev.io'
+                    <Box mt='0' ml='10%' p='8'>
+                        <Box
+                            maxW={['auto', '400']}
+                            height={['auto', '200']}
                             transition='0.5s'
-                            whiteSpace='nowrap'
                             transform={{
-                                md: "rotate(90deg) translateX(-100px)",
-                                lg: "rotate(0)",
+                                md: 'rotate(90deg) translateY(100px)',
+                                lg: 'rotate(0)',
                             }}
-                        />
+                        >
+                            <div className={styles.banner__text}>
+                                <div className={styles.banner__slash}></div>
+                                Tridev.io
+                            </div>
+                            <Text
+                                className={styles.banner__text}
+                                bgColor='#4549e0'
+                                _after={{
+                                    bgColor: '#4549e0',
+                                }}
+                                overflow='visible'
+                            >
+                                Tridev.io
+                            </Text>
+                            <Text
+                                overflow='revert'
+                                className={styles.banner__text}
+                                bgColor='#4549e0'
+                                _after={{
+                                    bgColor: '#4549e0',
+                                }}
+                            >
+                                Tridev.io
+                            </Text>
+                        </Box>
                     </Box>
                 </GridItem>
                 <GridItem
-                    colSpan={["12", "12", "8", "6"]}
+                    colSpan={['12', '12', '8', '6']}
                     h='100vh'
                     bgColor='white'
                     zIndex='4'
